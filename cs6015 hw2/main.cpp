f21bca796fe43ad094162dc2652025b434426fc9
Xiyao Xu <a710850142@gmail.com>
2024-01-23 23:42:23 -0700

    add

--- cs6015 hw2/main.cpp
+++ cs6015 hw2/main.cpp
@@ -1,2 +1,16 @@
-#define CATCH_CONFIG_MAIN
+#include "cmdline.h"
+#define CATCH_CONFIG_RUNNER
 #include "catch.h"
+#include <cstring>
+
+int main(int argc, char **argv) {
+
+    use_arguments(argc, argv);
+
+    if (Catch::Session().run() != 0) {
+        exit(1);
+    };
+
+
+    return 0;
+}
